d907eac56cfd889a9272e2590c53935c95146169
Alex-Gurung <aag1234@gmail.com>
2016-04-08 17:31:52 -0400

    Added python checks to nzherald

--- nzherald.py
+++ nzherald.py
@@ -1,11 +1,11 @@
 from bs4 import BeautifulSoup, SoupStrainer
-import urllib.request
+import urllib.request #PYTHON CHECK: if using python 2, import urllib2
 
 def main():
     out = open('output.txt', 'w') #Opens two files, one for reading in urls, and one for printing output data
     inp = open('nzurllist.txt', 'r')
     for u in inp:
-        opener = urllib.request.build_opener()
+        opener = urllib.request.build_opener() #PYTHON CHECK: if using python 2, say urllib2 instead of urllib.request
         opener.addheaders = [('User-agent', 'Mozilla/5.0')] #Mozilla thing may not be nessecary
         url = u #Not necessary but makes it easier to hard code a url
         soup = BeautifulSoup(opener.open(url), "html.parser")

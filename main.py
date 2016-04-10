e57fae3a7398de060a6d317278024b02a8afd6cc
Alex-Gurung <aag1234@gmail.com>
2016-04-10 17:08:01 -0400

    Changed to input which website, and then url

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def main():
-	number = input("Input website(cnn, nytimes, bbc, nzherald): ") 
-	scraper(number)
-def scraper(arg):
-	print(arg)
+	website = input("Input website(cnn, nytimes, bbc, nzherald): ") 
+	url = input("Input url: ")
+	scraper(website, url)
+def scraper(website, url):
+	print("%s, %s" % (website, url))
 if __name__ == '__main__':   
      main()

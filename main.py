aee5bee79bfdb57af03724435f0bda763d6172db
Alex-Gurung <aag1234@gmail.com>
2016-04-10 15:44:26 -0400

    Added a for loop and a helper method

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def main():
-	inp = input("Input text: ")
-	print(inp)
+	number = 0
+	number = int(input("Input number of urls: ")) #Has to be a proper number
+	for i in range(number):
+		inp = input("%s: url: " % i)
+		scraper(inp)
+def scraper(arg):
+	print(arg)
 if __name__ == '__main__':   
      main()

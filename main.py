6db368d576bedce038913a3be25b589661a37003
Alex-Gurung <aag1234@gmail.com>
2016-08-07 10:23:00 +0100

    Reran with bbc and commented the errors

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def scraper(website, url): #Function to complete the site specific scraping
 	inp = open(website+"urllist.txt", "w")
 	inp.write(url)
 	inp.close() #Unnessecary, but makes certain there isn't an issue
-	if (website == "bbc"):
+	if (website == "bbc"): #Current state: Correctly gets title but has trouble getting anything else
 		bbc.main()
 	if (website == "cnn"):
 		cnn.main()
